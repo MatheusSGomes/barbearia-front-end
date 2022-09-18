4bc66f50cdf93d1771cdc35f4dcf6934065ed35a
Matheus Gomes <math.s.gomes@gmail.com>
2022-09-18 20:08:17 -0300

    Adicionada mensagens de sucesso ou falha na confirmação do agendamento

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,7 @@
 const popup = document.querySelector('.popup');
-
 const btnAgendar = document.querySelector('.btn-agendar');
+const btnConfirmar = document.querySelector('.btn-popup-services');
+const popupForm = document.querySelector('.popup-form');
 
 function openPopup() {
   popup.style.display = 'flex';
@@ -13,6 +14,17 @@ function closePopup() {
 
 btnAgendar.addEventListener('click', (e) => {
   e.preventDefault();
-
   openPopup();
 });
+
+btnConfirmar.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  const iconSuccessful = document.querySelector('.popup-successful');
+  iconSuccessful.style.display = 'block';
+
+  const iconError = document.querySelector('.popup-error');
+  iconError.style.display = 'block';
+
+  popupForm.style.display = 'none';
+})
