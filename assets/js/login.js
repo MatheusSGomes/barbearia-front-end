c59f8706752f1d8448a69abb035be01722868ec0
Matheus Gomes <math.s.gomes@gmail.com>
2022-09-22 17:42:48 -0300

    Adicionada headers com chave de autorização para buscar usuários

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,4 @@
-const inputEmail = document.querySelector('#email');
-const inputPassword = document.querySelector('#password');
+import Cookie from './HandleCookies.js';
 
 // LOGIN
 // axios
@@ -8,12 +7,13 @@ const inputPassword = document.querySelector('#password');
 //     "password": "12345678",
 //   })
 //   .then(function (response) {
-//     console.log(response.data);
-//     console.log(response);
+//     Cookie
+//       .setCookie('token', response.headers.authorization, 7);
 //   })
 //   .catch(function (error) {
 //     console.log(error);
 //   });
+
 
 // REGISTER
 // axios
@@ -30,3 +30,31 @@ const inputPassword = document.querySelector('#password');
 //     console.log(error);
 //   });
 
+
+// INDEX - LISTAR USUÁRIOS
+// axios
+//   .get('http://127.0.0.1:8000/api/usuario', {
+//     headers: {
+//       "authorization": Cookie.getCookie('token')
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+
+// SHOW - LISTAR UM USUÁRIO
+// axios
+//   .get('http://127.0.0.1:8000/api/usuario/4', { 
+//     headers: {
+//       "authorization": Cookie.getCookie('token')
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response.data)
+//   })
+//   .catch(function (error) {
+//     console.log(error)
+//   });
